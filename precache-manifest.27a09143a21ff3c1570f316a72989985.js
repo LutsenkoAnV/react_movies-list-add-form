825adbcb5429c5766a62fcaf36eb7f124a9770c7
LutsenkoAnV <48694313+LutsenkoAnV@users.noreply.github.com>
2020-02-21 13:39:57 +0200

    Updates

---
+++ precache-manifest.27a09143a21ff3c1570f316a72989985.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "329a0c209fa47667b94ad137dff88828",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5f62baf4b30729a8e1eb",
+    "url": "./static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "05e9f8c5daf004995540",
+    "url": "./static/css/main.67ccd2fd.chunk.css"
+  },
+  {
+    "revision": "5f62baf4b30729a8e1eb",
+    "url": "./static/js/2.8126ff9f.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "./static/js/2.8126ff9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05e9f8c5daf004995540",
+    "url": "./static/js/main.bf709398.chunk.js"
+  },
+  {
+    "revision": "bd651092f6acbe222378",
+    "url": "./static/js/runtime-main.14f95850.js"
+  }
+]);
